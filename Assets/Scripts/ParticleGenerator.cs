254694be80420f2c4b4993b7ef16e1228d7435b1
Jansen Samosa <jansen.samosa2@gmail.com>
2025-04-15 18:58:41 -0700

    Restructuring

---
+++ Assets/Scripts/ParticleGenerator.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ParticleGenerator : MonoBehaviour
+{
+    public GameObject ParticlePrefab;
+ 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    void Update() {
+        
+    }
+}
