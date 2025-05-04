5bde12b282f36ceb6d0546430697bef2c95dd698
Jansen Samosa <jansen.samosa2@gmail.com>
2025-05-03 21:43:07 -0700

    Particle Rendering optimization, wind simulation, interactive light, custom particle lit shader

---
+++ Assets/Scripts/DirectionalLightInteraction.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class DirectionalLightInteraction : MonoBehaviour
+{
+    public float intensityAdjustmentSensitivity = .2f;
+
+    private Camera cam;                  // assign in Inspector or will use Camera.main
+    private Light directionalLight;
+
+    void Start() {
+        cam = Camera.main;
+        directionalLight = GetComponent<Light>();
+    }
+
+    void Update()
+    {   
+        if (Input.GetMouseButton(1)) {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity)) {
+                Vector3 point = hit.point;
+                transform.LookAt(point);
+            }
+        }
+        
+        float intensityAdjustment = Input.GetAxis("Mouse ScrollWheel") * intensityAdjustmentSensitivity;
+        directionalLight.intensity += intensityAdjustment;
+    }
+}
---
+++ Assets/Scripts/ParticleSystemOptimized.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[System.Serializable]
+public struct ParticleData
+{   
+    public Vector3 position;
+    public Vector3 scale;
+    public Quaternion rotation;
+
+    public Vector3 velocity;
+    
+    public bool settled;
+    public float activeLifetime;
+}
+
+public class ParticleSystemOptimized : MonoBehaviour
+{   
+
+    public Material particleMaterial;
+    public Mesh particleMesh;
+    public bool renderAsBillboardTexture = true;
+
+    public Transform boundingBox;
+    public float scaleMin = 1;
+    public float scaleMax = 1;
+
+    public float maxVelocity = 1;
+
+    public Vector3 addedForce = new Vector3(0, -0.01f, 0);
+    public float drag = 0.01f;
+    
+    public int spawnCount = 100;
+    public float particleActiveLifetime = 5;
+
+    public List<ParticleData> activeParticles = new List<ParticleData>();   
+    public List<ParticleData> settledParticles = new List<ParticleData>();
+    private List<Matrix4x4> settledParticlesTransformations = new List<Matrix4x4>();
+
+    private Vector3 boundMin;
+    private Vector3 boundMax;
+
+    public WindField windField;
+
+    void Start() {
+        // set the particle emission bounds
+        boundMin = boundingBox.position - boundingBox.localScale/2;
+        boundMax = boundingBox.position + boundingBox.localScale/2;     
+
+        // enable instancing for material
+        particleMaterial.enableInstancing = true;
+
+        // spawn particles
+        for (int i = 0; i < spawnCount; i++) {
+            SpawnParticleRandomly();
+        }   
+    }
+
+    void Update() {   
+        // transformation of each particle
+        Matrix4x4[] activeParticleTransformations = new Matrix4x4[activeParticles.Count];
+        
+        // UPDATE PARTICLES
+        for (int i = 0; i < activeParticles.Count; i++) {
+            ParticleData p = activeParticles[i];
+            
+            // rotate billboard particles towards camera
+            if (renderAsBillboardTexture) {
+                Vector3 toCam = p.position - Camera.main.transform.position;
+                Quaternion billRot = Quaternion.LookRotation(toCam);
+                p.rotation = billRot;
+            }
+            
+            // update particle life time
+            p.activeLifetime -= Time.deltaTime;
+
+            // apply gravity
+            p.velocity += addedForce * Time.deltaTime;
+            
+            // apply wind
+            if (windField) {
+                Vector3 curWind = windField.SampleWind(p.position);
+                p.velocity += curWind * Time.deltaTime;
+            }
+
+            // constrain velocity
+            p.velocity = Vector3.ClampMagnitude(p.velocity, maxVelocity);
+
+            // apply drag
+            p.velocity -= (p.velocity * drag * Time.deltaTime);
+            
+            Vector3 nextPos = p.position + p.velocity * Time.deltaTime;
+            if (Physics.Linecast(p.position, nextPos, out RaycastHit hit)) {
+                p.position = hit.point;      // snap to contact
+                p.velocity = Vector3.zero;   // zero out so they “settle”
+                p.settled = true;
+                settledParticles.Add(p);
+                settledParticlesTransformations.Add(Matrix4x4.TRS(p.position, p.rotation, p.scale));
+            }
+            else {
+                p.position = nextPos;
+            }
+
+            activeParticleTransformations[i] = Matrix4x4.TRS(p.position, p.rotation, p.scale);
+            activeParticles[i] = p;
+        }
+
+        // render all particles
+        if (activeParticles.Count > 0) {
+            RenderParams rp = new RenderParams(particleMaterial);
+            Graphics.RenderMeshInstanced(rp, particleMesh, 0, activeParticleTransformations);
+        }
+
+        if (settledParticles.Count > 0) {
+            RenderParams rp = new RenderParams(particleMaterial);
+            Graphics.RenderMeshInstanced(rp, particleMesh, 0, settledParticlesTransformations.ToArray());
+        }
+
+
+        activeParticles.RemoveAll(item => item.settled);
+        activeParticles.RemoveAll(item => item.activeLifetime <= 0);
+
+        // DEBUGGING CONTROLS
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            for (int i = 0; i < spawnCount; i++) {
+                SpawnParticleRandomly();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.R)) {
+            activeParticles.Clear();
+            settledParticles.Clear();
+            settledParticlesTransformations.Clear();
+        }
+    }
+        
+    private void SpawnParticleRandomly() {
+        Vector3 randomPos = new Vector3(
+            UnityEngine.Random.Range(boundMin.x, boundMax.x),
+            UnityEngine.Random.Range(boundMin.y, boundMax.y),
+            UnityEngine.Random.Range(boundMin.z, boundMax.z)
+        );
+        float randomScale = UnityEngine.Random.Range(scaleMin, scaleMax);
+
+        Vector3 randomVelocity = new Vector3(
+            UnityEngine.Random.Range(-0.1f, 0.1f),
+            UnityEngine.Random.Range(-0.1f, 0.1f),
+            UnityEngine.Random.Range(-0.1f, 0.1f)
+        );
+
+        SpawnParticle(randomPos, randomScale, randomVelocity);
+    }
+
+    // spawns a particle in the world and adds a reference to the active particles list
+    private void SpawnParticle(Vector3 pos, float scale = 1, Vector3 initialVelocity = default) {
+        ParticleData newParticle = new ParticleData();
+        newParticle.position = pos;
+        newParticle.rotation = Quaternion.identity;
+        newParticle.scale = new Vector3(scale, scale, scale);
+        
+        newParticle.velocity = initialVelocity;
+        newParticle.settled = false;
+        newParticle.activeLifetime = particleActiveLifetime;
+
+        activeParticles.Add(newParticle);
+    }    
+}
