877e9afbdb4fe716bae3883791317f851c1e36b5
Jansen Samosa <jansen.samosa2@gmail.com>
2025-05-04 12:25:34 -0700

    Added V-Light

--- Assets/Scripts/DirectionalLightInteraction.cs
+++ Assets/Scripts/DirectionalLightInteraction.cs
@@ -2,14 +2,19 @@ using UnityEngine;
 
 public class DirectionalLightInteraction : MonoBehaviour
 {
+    public VLight vlight;
     public float intensityAdjustmentSensitivity = .2f;
 
     private Camera cam;                  // assign in Inspector or will use Camera.main
     private Light directionalLight;
+    
+    // retio of intensity of vlight and directional light.
+    private float lightIntensityRatio;
 
     void Start() {
         cam = Camera.main;
         directionalLight = GetComponent<Light>();
+        lightIntensityRatio = vlight.lightMultiplier / directionalLight.intensity;
     }
 
     void Update()
@@ -19,10 +24,13 @@ public class DirectionalLightInteraction : MonoBehaviour
             if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity)) {
                 Vector3 point = hit.point;
                 transform.LookAt(point);
+                vlight.transform.position = transform.position;
+                vlight.transform.LookAt(point);
             }
         }
         
         float intensityAdjustment = Input.GetAxis("Mouse ScrollWheel") * intensityAdjustmentSensitivity;
         directionalLight.intensity += intensityAdjustment;
+        vlight.lightMultiplier = directionalLight.intensity * lightIntensityRatio;
     }
 }
